e8915d50f9447454a87846529dad05d12d0a0e17
NateOwl <natedog112004@gmail.com>
2020-09-04 03:48:22 +0000

    queue finished and assignment 5 problems linted

--- completed_assignment_5/queue.py
+++ completed_assignment_5/queue.py
@@ -1,33 +1,43 @@
 class Queue():
-  
-  def __init__(self):
-    self.data=[]
 
-  def enqueue(self,value):
-    self.data.append(value)
+    def __init__(self):
+        self.data = []
 
-  def dequeue(self):
-    self.data.pop(0)
+    def enqueue(self, value):
+        self.data.append(value)
 
-  def peek(self):
-    return self.data[0]
+    def dequeue(self):
+        self.data.pop(0)
+
+    def peek(self):
+        return self.data[0]
 
 q = Queue()
-q.data
-assert(q.data) == []
+print('testing for init')
+print(q.data)
+assert(q.data) == [], "answer should have been {}".format([])
+print('passed')
 
 q.enqueue('a')
 q.enqueue('b')
 q.enqueue('c')
+print('testing for enqueue')
 print(q.data)
-#assert q.data == ['a', 'b', 'c']
+assert q.data == ['a', 'b', 'c'], "answer should have been {}".format(['a', 'b', 'c'])
+print('Passed')
 
 q.dequeue()
+print('testing for dequeue')
 print(q.data)
-assert q.data == ['b', 'c']
+assert q.data == ['b', 'c'], "answer should have been {}".format(['b', 'c'])
+print('passed')
 
+print('testing for peek')
 q.peek()
-assert q.peek() == 'b'
+assert q.peek() == 'b', "answer should have been 'b'"
+print('passed')
 
+print('testing for data')
 print(q.data)
-assert q.data == ['b', 'c']
+assert q.data == ['b', 'c'], "answer should have been {}".format(['b', 'c'])
+print('passed')
--- completed_assignment_5/root_approximation.py
+++ completed_assignment_5/root_approximation.py
@@ -1,14 +1,14 @@
 def update_bounds(bounds):
-  
-  midpoint=(bounds[0]+bounds[1])/2
 
-  if midpoint**2 > 2:
-    bounds[1] = midpoint
-  
-  else:
-    bounds[0] = midpoint
-  
-  return bounds
+    midpoint = (bounds[0]+bounds[1]) / 2
+
+    if midpoint**2 > 2:
+        bounds[1] = midpoint
+
+    else:
+        bounds[0] = midpoint
+
+    return bounds
 
 print(update_bounds([1, 2]))
 
@@ -22,13 +22,14 @@ assert update_bounds([1, 1.5]) == [1.25, 1.5], "Answer was supposed to be [1.25,
 
 print('Passed')
 
+
 def estimate_root(precision, bounds):
-  
-  while(bounds[1]-bounds[0]) > precision:
-    bounds = update_bounds(bounds)
-    print(bounds)
-  
-  return bounds
+
+    while(bounds[1]-bounds[0]) > precision:
+        bounds = update_bounds(bounds)
+        print(bounds)
+
+    return bounds
 
 assert estimate_root(.1, [1, 2]) == [1.375, 1.4375],    "answer was suppose to be [1.375, 1.4375]"
 
