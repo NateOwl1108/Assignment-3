7aa5e41c1aeb591eaf87937ef5f447ca142990df
NateOwl <natedog112004@gmail.com>
2020-10-23 05:53:39 +0000

    I fixed up the functions so the min is correct for gradient descent

--- completed_assignment_22/gradient_descent.py
+++ completed_assignment_22/gradient_descent.py
@@ -5,27 +5,26 @@ def f(x):
 
 def gradient_descent(f,x_0,alpha,δ,iterations):
   f_prime_initial = (f(x_0 + .5 * δ) - f(x_0 - .5 * δ)) / δ
-  print
-  x_n = 0
-  x_n_= x_0 - alpha * f_prime_initial
+  alpha_times_f_prime = alpha * f_prime_initial
+  x_n = x_0 - alpha * f_prime_initial
   while iterations > 0:
     f_prime = (f(x_n + .5 * δ) - f(x_n - .5 * δ)) / δ
-    x_n -= alpha * f_prime_initial
+    x_n= x_n - alpha * f_prime
     iterations -= 1
   return x_n
 
 print(gradient_descent(f,0,0.01,0.0001,10000))
 
 def f(x):
-  return (x**2 + math.cos(x))/math.sin(x)
+  return (x**2 + math.cos(x))/(2.71828**math.sin(x))
 
 def gradient_descent(f,x_0,alpha,δ,iterations):
   f_prime_initial = (f(x_0 + .5 * δ) - f(x_0 - .5 * δ)) / δ
-  x_n = 0
-  x_n_= x_0 - alpha * f_prime_initial
+  alpha_times_f_prime = alpha * f_prime_initial
+  x_n = x_0 - alpha * f_prime_initial
   while iterations > 0:
     f_prime = (f(x_n + .5 * δ) - f(x_n - .5 * δ)) / δ
-    x_n -= alpha * f_prime_initial
+    x_n= x_n - alpha * f_prime
     iterations -= 1
   return x_n
 
