7c98732152d73cacd869724c6378a5b28d6aa644
NateOwl <>
2021-06-12 00:17:28 +0000

    hash table completed

---
+++ hash_table.py
@@ -0,0 +1,47 @@
+
+
+def hash_function(string):
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    sum_string = 0
+    for char in string:
+      sum_string += alphabet.index(char)
+    return sum_string
+
+def insert(array_input, key, value):
+    index = hash_function(key)
+    if array_input[index] in array_input:
+      array_input[index].append((key,value))
+    return array_input
+
+def find(input_array, key):
+    index = hash_function(key)
+    for group in intput_array[index]:
+      if key in group:
+        return(group)
+
+print(array)
+array = [[], [], [], [], []]
+
+insert(array, 'a', [0,1])
+insert(array, 'b', 'abcd')
+insert(array, 'c', 3.14)
+print(array)
+[[('a',[0,1])], [('b','abcd')], [('c',3.14)], [], []]
+
+insert(array, 'd', 0)
+insert(array, 'e', 0)
+insert(array, 'f', 0)
+print(array)
+[[('a',[0,1]), ('f',0)], [('b','abcd')], [('c',3.14)], [('d',0)], [('e',0)]]
+
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+for i, char in enumerate(alphabet):
+    key = 'someletters'+char
+    value = [i, i**2, i**3]
+    insert(array, key, value)
+
+for i, char in enumerate(alphabet):
+    key = 'someletters'+char
+    output_value = find(array, key)
+    desired_value = [i, i**2, i**3]
+    assert output_value == desired_value
--- main.sh
+++ main.sh
@@ -1,5 +1,5 @@
 #python assignment_36_grid_search.py
 #python assignment_33_graph.py
 #python Assignments_20-29/completed_assignment_23/merge_sort.py
-python magic_square.py
-
+#python magic_square.py
+python hash_table.py
